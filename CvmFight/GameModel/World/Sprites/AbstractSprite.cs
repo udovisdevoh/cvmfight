c3c6796f9fc059def099aee88d3626e32223be50
glacasse <glacasse@e78b9bc0-2561-11df-a3ec-b365d55abd7c>
2010-03-01 21:21:05 +0000

    Model: Added sprites, sprite pool and world

---
+++ CvmFight/GameModel/World/Sprites/AbstractSprite.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CvmFight
+{
+    /// <summary>
+    /// Abstract sprite
+    /// </summary>
+    class AbstractSprite
+    {
+        #region Fields
+        /// <summary>
+        /// x coordinate position
+        /// </summary>
+        private double positionX = 0.0;
+
+        /// <summary>
+        /// y coordinate position
+        /// </summary>
+        private double positionY = 0.0;
+
+        /// <summary>
+        /// height from the ground
+        /// </summary>
+        private double positionZ = 0.0;
+
+        /// <summary>
+        /// sprite's angle
+        /// </summary>
+        private double angleRadian = 0.0;
+
+        /// <summary>
+        /// sprite's radius
+        /// </summary>
+        private double radius = 0.0;
+
+        /// <summary>
+        /// sprite's height
+        /// </summary>
+        private double height = 0.0;
+
+        /// <summary>
+        /// sprite's weight
+        /// </summary>
+        private double weight = 0.0;
+
+        /// <summary>
+        /// sprite's velocity
+        /// </summary>
+        private double velocity = 0.0;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// x coordinate position
+        /// </summary>
+        public double PositionX
+        {
+            get { return positionX; }
+            set { positionX = value; }
+        }
+
+        /// <summary>
+        /// y coordinate position
+        /// </summary>
+        public double PositionY
+        {
+            get { return positionY; }
+            set { positionY = value; }
+        }
+
+        /// <summary>
+        /// z coordinate position
+        /// </summary>
+        public double PositionZ
+        {
+            get { return positionZ; }
+            set { positionZ = value; }
+        }
+
+        /// <summary>
+        /// Return angle in radian from -1 to 1
+        /// </summary>
+        public double AngleRadian
+        {
+            get
+            {
+                return angleRadian;
+            }
+
+            set
+            {
+                while (value >= Math.PI * 2)
+                    value -= Math.PI * 2;
+
+                while (value < 0)
+                    value += Math.PI * 2;
+
+                angleRadian = value;
+            }
+        }
+
+        /// <summary>
+        /// Angle in degree (from 0 to 360)
+        /// </summary>
+        public double AngleDegree
+        {
+            get
+            {
+                return angleRadian / Math.PI * 180.0;
+            }
+
+            set
+            {
+                AngleRadian = value / 180.0 * Math.PI;
+            }
+        }
+
+        /// <summary>
+        /// Sprite's radius
+        /// </summary>
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        /// <summary>
+        /// Sprite's diameter
+        /// </summary>
+        public double Diameter
+        {
+            get { return radius * 2; }
+            set { radius = value / 2; }
+        }
+
+        /// <summary>
+        /// Sprite's height
+        /// </summary>
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        /// <summary>
+        /// Sprite's weight
+        /// </summary>
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        /// <summary>
+        /// Sprite's velocity
+        /// </summary>
+        public double Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+        #endregion
+    }
+}
---
+++ CvmFight/GameModel/World/World.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CvmFight
+{
+    /// <summary>
+    /// Represents the world
+    /// </summary>
+    class World
+    {
+        #region Parts
+        /// <summary>
+        /// Map
+        /// </summary>
+        private AbstractMap map;
+
+        /// <summary>
+        /// Sprite pool
+        /// </summary>
+        private SpritePool spritePool;
+        #endregion
+    }
+}
