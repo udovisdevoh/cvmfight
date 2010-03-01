71cb4e8e0b40660cbf9a9f1cf5f5d0dd751a7e67
glacasse <glacasse@e78b9bc0-2561-11df-a3ec-b365d55abd7c>
2010-03-01 23:59:38 +0000

    Déplacement en cours

---
+++ CvmFight/CvmFight/GameModel/Waves/WaveCache.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Waves
+{
+    class WaveCache
+    {
+        #region Fields
+        private Dictionary<double, double> internalCache = new Dictionary<double, double>();
+        #endregion
+
+        #region Public Methods
+        public bool ContainsKey(double x)
+        {
+            return internalCache.ContainsKey(x);
+        }
+
+        public void Add(double x, double value)
+        {
+            if (!internalCache.ContainsKey(x))
+                internalCache.Add(x, value);
+        }
+
+        public double Get(double x)
+        {
+            return internalCache[x];
+        }
+        #endregion
+    }
+}
---
+++ CvmFight/CvmFight/GameModel/World/World.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CvmFight
+{
+    /// <summary>
+    /// Represents the world
+    /// </summary>
+    class World
+    {
+        #region Fields
+        private Player currentPlayer;
+        #endregion
+
+        #region Parts
+        /// <summary>
+        /// Map
+        /// </summary>
+        private AbstractMap map;
+
+        /// <summary>
+        /// Sprite pool
+        /// </summary>
+        private SpritePool spritePool;
+        #endregion
+
+        #region Constructor
+        public World()
+        {
+            currentPlayer = new Player();
+            map = new HardCodedMap();
+            spritePool = new SpritePool();
+            spritePool.Add(currentPlayer);
+        }
+        #endregion
+    }
+}
