1a55ddb3f874f409ed9b0f84772e74761d516d1c
glacasse <glacasse@e78b9bc0-2561-11df-a3ec-b365d55abd7c>
2010-03-01 20:14:33 +0000

    Added map generator and wave builder (wave maps, hard coded maps) etc

---
+++ CvmFight/GameModel/Waves/IWave.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Waves
+{
+    interface IWave : IEquatable<IWave>
+    {
+        /// <summary>
+        /// Get amplitude momentum Y value at X
+        /// </summary>
+        /// <param name="x">x coordinates</param>
+        /// <returns>amplitude momentum Y value at X</returns>
+        double GetYValueAt(double x);
+
+        /// <summary>
+        /// Normalize the wave
+        /// </summary>
+        /// <returns>Normalized wave</returns>
+        void Normalize();
+    }
+}
