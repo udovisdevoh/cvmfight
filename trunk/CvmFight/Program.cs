fdf4dad875739d03eb31f170017d7f29d4884cc7
glacasse <glacasse@e78b9bc0-2561-11df-a3ec-b365d55abd7c>
2010-03-01 22:09:04 +0000

    Putted back dlls

--- trunk/CvmFight/Program.cs
+++ trunk/CvmFight/Program.cs
@@ -1,14 +1,43 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SdlDotNet.Graphics;
+using SdlDotNet.Core;
 
 namespace CvmFight
 {
     class Program
     {
+        private Surface screen;
+
+        private Random random = new Random();
+        
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.initialisation();
         }
+
+        public void initialisation()
+        {
+            screen = new Surface(1024, 768);
+            screen = Video.SetVideoMode(1024, 768);
+            Events.TargetFps = 40;
+            Events.Tick += run;
+            Events.Run();
+        }
+
+        public void run(object sender, TickEventArgs args)
+        {
+            if (random.Next(2) == 0)
+                screen.Fill(Color.Beige);
+            else
+                screen.Fill(Color.Blue);
+
+            screen.Update();
+        }
+      
     }
 }
