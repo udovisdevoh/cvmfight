2833092669358c45d06309668b16d324671bdbbf
glacasse <glacasse@e78b9bc0-2561-11df-a3ec-b365d55abd7c>
2010-03-02 22:15:48 +0000

    Implemented cached wave map

---
+++ CvmFight/CvmFight/GameModel/World/Map/MapTypes/CachedWaveMap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CvmFight
+{
+    class CachedWaveMap : AbstractMap
+    {
+        #region Constants
+        private double precision = 0.01;
+        #endregion
+
+        #region Fields
+        private WaveMap waveMap;
+
+        private WaveMapCache waveMapCache;
+        #endregion
+
+        #region Public Methods
+        public CachedWaveMap(Random random)
+        {
+            waveMap = new WaveMap(random);
+            waveMapCache = new WaveMapCache(waveMap, precision);
+        }
+
+        public override AbstractMatterType GetMatterTypeAt(double x, double y)
+        {
+            return waveMapCache.GetValueAt(x, y);
+        }
+
+        public override int Width
+        {
+            get { return waveMap.Width; }
+        }
+
+        public override int Height
+        {
+            get { return waveMap.Height; }
+        }
+        #endregion
+    }
+}
