c9318ccc94bbc3c83a8c470652efbf34375288bf
glacasse <glacasse@e78b9bc0-2561-11df-a3ec-b365d55abd7c>
2010-03-02 19:24:12 +0000

    Events: work in progress

--- CvmFight/CvmFight/Program.cs
+++ CvmFight/CvmFight/Program.cs
@@ -11,26 +11,60 @@ namespace CvmFight
 {
     class Program
     {
+        #region Fields and parts
         private World world = new World();
 
         private AbstractGameViewer gameViewer = new MiniMap();
 
-        public static void Main(string[] args)
-        {
-            Program program = new Program();
-            program.Start();
-        }
+        private KeyBoardInput keyBoardInput = new KeyboardInput();
+        #endregion
 
+        #region Public Methods
         public void Start()
         {
             Events.TargetFps = 60;
             Events.Tick += Update;
             Events.Run();
+            Events.KeyboardDown += OnKeyboardDown;
+            Events.KeyboardUp += OnKeyboardUp;
         }
 
         public void Update(object sender, TickEventArgs args)
         {
             gameViewer.Update(world);
         }
+
+        public void OnKeyboardDown(object sender, KeyboardEventArgs args)
+        {
+            if (args.Key == Key.UpArrow || args.Key == Key.W)
+                keyBoardInput.IsPressUp = true;
+            else if (args.Key == Key.DownArrow || args.Key == Key.S)
+                keyBoardInput.IsPressDown = true;
+            else if (args.Key == Key.LeftArrow || args.Key == Key.A)
+                keyBoardInput.IsPressLeft = true;
+            else if (args.Key == Key.RightArrow || args.Key == Key.D)
+                keyBoardInput.IsPressRight = true;
+        }
+
+        public void OnKeyboardUp(object sender, KeyboardEventArgs args)
+        {
+            if (args.Key == Key.UpArrow || args.Key == Key.W)
+                keyBoardInput.IsPressUp = false;
+            else if (args.Key == Key.DownArrow || args.Key == Key.S)
+                keyBoardInput.IsPressDown = false;
+            else if (args.Key == Key.LeftArrow || args.Key == Key.A)
+                keyBoardInput.IsPressLeft = false;
+            else if (args.Key == Key.RightArrow || args.Key == Key.D)
+                keyBoardInput.IsPressRight = false;
+        }
+        #endregion
+
+        #region Static
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+        }
+        #endregion
     }
 }
