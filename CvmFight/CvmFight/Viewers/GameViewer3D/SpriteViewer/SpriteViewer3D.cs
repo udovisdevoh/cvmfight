298f36b631d29875698ee7d9c3470499b18b8a13
glacasse <glacasse@e78b9bc0-2561-11df-a3ec-b365d55abd7c>
2010-03-04 00:06:58 +0000

    3D: Added sprite viewer stub

---
+++ CvmFight/CvmFight/Viewers/GameViewer3D/SpriteViewer/SpriteViewer3D.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SdlDotNet.Graphics;
+using SdlDotNet.Core;
+using SdlDotNet.Graphics.Primitives;
+
+namespace CvmFight
+{
+    class SpriteViewer3D
+    {
+        #region Fields
+        private int screenWidth;
+
+        private int screenHeight;
+        #endregion
+
+        #region Constructor
+        public SpriteViewer3D(int screenWidth, int screenHeight)
+        {
+            this.screenWidth = screenWidth;
+            this.screenHeight = screenHeight;
+        }
+        #endregion
+
+        #region Public Methods
+        public void ViewSprite(AbstractSprite sprite, Surface mainSurface)
+        {
+        }
+        #endregion
+    }
+}
