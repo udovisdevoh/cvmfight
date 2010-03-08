79fe8645429939a1073bc439b2dac2eb36efe3fe
glacasse <glacasse@e78b9bc0-2561-11df-a3ec-b365d55abd7c>
2010-03-08 20:16:22 +0000

    Added sprite action cycles Hud: added attack animations

--- CvmFight/CvmFight/Viewers/GameViewer3D/Hud/Hud.cs
+++ CvmFight/CvmFight/Viewers/GameViewer3D/Hud/Hud.cs
@@ -26,6 +26,16 @@ namespace CvmFight
         private Surface healthLabel;
 
         private Surface fragLabel;
+
+        private Surface restFist;
+
+        private Surface attackFist;
+
+        private Surface restKick;
+
+        private Surface midKick;
+
+        private Surface attackKick;
         #endregion
 
         #region Constructor
@@ -36,6 +46,53 @@ namespace CvmFight
 
             healthLabel = labelFont.Render("health", System.Drawing.Color.White);
             fragLabel = labelFont.Render("frags", System.Drawing.Color.White);
+
+            restFist = BuildRestFistSurface();
+            attackFist = BuildAttackFistSurface();
+
+            restKick = BuildRestKickSurface();
+            midKick = BuildMidKickSurface();
+            attackKick = BuildAttackKickSurface();
+        }
+
+        private Surface BuildRestKickSurface()
+        {
+            Surface surface = new Surface("Assets/Hud/kick001.png");
+            surface.Transparent = true;
+            surface = surface.CreateScaledSurface((double)screenHeight / 3 / (double)surface.Height, true);
+            return surface;
+        }
+
+        private Surface BuildMidKickSurface()
+        {
+            Surface surface = new Surface("Assets/Hud/kick002.png");
+            surface.Transparent = true;
+            surface = surface.CreateScaledSurface((double)screenHeight / 3 / (double)surface.Height, true);
+            return surface;
+        }
+
+        private Surface BuildAttackKickSurface()
+        {
+            Surface surface = new Surface("Assets/Hud/kick003.png");
+            surface.Transparent = true;
+            surface = surface.CreateScaledSurface((double)screenHeight / 3 / (double)surface.Height, true);
+            return surface;
+        }
+
+        private Surface BuildAttackFistSurface()
+        {
+            Surface surface = new Surface("Assets/Hud/punch002.png");
+            surface.Transparent = true;
+            surface = surface.CreateScaledSurface((double)screenHeight / 1.7 / (double)surface.Height,true);
+            return surface;
+        }
+
+        private Surface BuildRestFistSurface()
+        {
+            Surface surface = new Surface("Assets/Hud/punch001.png");
+            surface.Transparent = true;
+            surface = surface.CreateScaledSurface((double)screenHeight / 2.5 / (double)surface.Height,true);
+            return surface;
         }
         #endregion
 
@@ -44,9 +101,38 @@ namespace CvmFight
         {
             if (health == null)
                 health = bigRed.Render(player.Health.ToString(), System.Drawing.Color.Red);
-            
+
             if (fragCount == null)
                 fragCount = bigRed.Render(player.FragCount.ToString(), System.Drawing.Color.Red);
+
+
+            int attackCycleState = player.AttackCycle.GetState();
+            if (player.IsCrouch || player.PositionZ > 0)
+            {
+                if (attackCycleState == 0)
+                {
+                    surface.Blit(restKick, PointLoader.GetPoint(screenWidth / 3, screenHeight - restKick.Height));
+                }
+                else if (attackCycleState == 1)
+                {
+                    surface.Blit(midKick, PointLoader.GetPoint(screenWidth / 3, screenHeight - midKick.Height));
+                }
+                else if (attackCycleState == 2)
+                {
+                    surface.Blit(attackKick, PointLoader.GetPoint(screenWidth / 3, screenHeight - attackKick.Height));
+                }
+            }
+            else
+            {
+                if (attackCycleState == 0)
+                {
+                    surface.Blit(restFist, PointLoader.GetPoint(0, screenHeight - restFist.Height));
+                }
+                else
+                {
+                    surface.Blit(attackFist, PointLoader.GetPoint(0, screenHeight - attackFist.Height));
+                }
+            }
 
             surface.Blit(healthLabel, PointLoader.GetPoint(screenWidth - 140, screenHeight - 80));
             surface.Blit(health, PointLoader.GetPoint(screenWidth - 140, screenHeight - 65));
