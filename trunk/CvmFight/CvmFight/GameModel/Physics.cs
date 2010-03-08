46947ff776a6caafedc7ee59dd4f10552e2f7830
glacasse <glacasse@e78b9bc0-2561-11df-a3ec-b365d55abd7c>
2010-03-08 18:48:59 +0000

    Added antialias to sprite scalling Increased max sprite size

--- trunk/CvmFight/CvmFight/GameModel/Physics.cs
+++ trunk/CvmFight/CvmFight/GameModel/Physics.cs
@@ -146,15 +146,15 @@ namespace CvmFight
             sprite.AngleRadian += ((double)angleRotationStrength / -200);
         }
 
-        public static void MakeJump(AbstractSprite sprite)
+        public static void MakeJump(AbstractSprite sprite, double timeDelta)
         {
             #warning, Time delta must be properly placed in jump and fall
             if (!sprite.IsNeedToJumpAgain)
             {
                 if (sprite.PositionZ <= 0)
-                    sprite.CurrentJumpAcceleration = sprite.MaxJumpAcceleration;
+                    sprite.CurrentJumpAcceleration = sprite.MaxJumpAcceleration + 0.1 * timeDelta;
                 else
-                    sprite.CurrentJumpAcceleration += 0.05;
+                    sprite.CurrentJumpAcceleration += 0.065 * timeDelta;
 
                 if (sprite.CurrentJumpAcceleration < 0)
                 {
@@ -166,9 +166,8 @@ namespace CvmFight
         public static void MakeFall(AbstractSprite sprite, double timeDelta)
         {
             #warning, Time delta must be properly placed in jump and fall
-            sprite.CurrentJumpAcceleration -= 0.1;
-            sprite.PositionZ += sprite.CurrentJumpAcceleration / 10 * timeDelta;
-
+            sprite.CurrentJumpAcceleration -= 0.1 * timeDelta;
+            sprite.PositionZ += sprite.CurrentJumpAcceleration / 10;
             sprite.PositionZ = Math.Max(0, sprite.PositionZ);           
         }
         #endregion
