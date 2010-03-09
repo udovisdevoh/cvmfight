9546da99c8ab24cc5c3501207516e5897ef745c6
glacasse <glacasse@e78b9bc0-2561-11df-a3ec-b365d55abd7c>
2010-03-09 21:58:30 +0000

    Removed constuctor from concrete sprite implementations

--- trunk/CvmFight/CvmFight/GameModel/World/Sprites/Player.cs
+++ trunk/CvmFight/CvmFight/GameModel/World/Sprites/Player.cs
@@ -7,26 +7,65 @@ namespace CvmFight
 {
     class Player : AbstractSprite
     {
-        #region Constructor
-        /// <summary>
-        /// Create player's sprite
-        /// </summary>
-        public Player()
+        #region Abstract Sprite Members
+        protected override double GetHeight()
         {
-            IsAlive = false;
-            Height = 1.0;
-            Radius = 0.1;
-            AttackRange = 0.4;
-            AttackAngleRange = 0.17;
-            AttackPower = 2.5;
-            DefaultHealth = 100.0;
-            Health = 0;
-            MaxHealth = 200.0;
-            MaxJumpAcceleration = 0.83;
-            CrouchSpeedMultiplier = 0.2;
-            DefaultWalkingDistance = 0.02;
-            JumpSpeedMultiplier = 1.6;
-            AttackWalkSpeedMultiplier = 0;
+            return 1.0;
+        }
+
+        protected override double GetRadius()
+        {
+            return 0.1;
+        }
+
+        protected override double GetAttackRange()
+        {
+            return 0.4;
+        }
+
+        protected override double GetAttackAngleRange()
+        {
+            return 0.17;
+        }
+
+        protected override double GetAttackPower()
+        {
+            return 2.5;
+        }
+
+        protected override double GetDefaultHealth()
+        {
+            return 100;
+        }
+
+        protected override double GetMaxHealth()
+        {
+            return 200;
+        }
+
+        protected override double GetMaxJumpAcceleration()
+        {
+            return 0.83;
+        }
+
+        protected override double GetCrouchSpeedMultiplier()
+        {
+            return 0.2;
+        }
+
+        protected override double GetDefaultWalkingDistance()
+        {
+            return 0.02;
+        }
+
+        protected override double GetJumpSpeedMultiplier()
+        {
+            return 1.6;
+        }
+
+        protected override double GetAttackWalkSpeedMultiplier()
+        {
+            return 0;
         }
         #endregion
     }
