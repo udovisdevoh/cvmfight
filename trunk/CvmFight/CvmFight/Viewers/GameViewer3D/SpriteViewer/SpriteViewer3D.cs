b05ee6fa6c56658dffb88af4d1b0f7103899043c
glacasse <glacasse@e78b9bc0-2561-11df-a3ec-b365d55abd7c>
2010-03-14 15:26:04 +0000

    Moved sprite folder and removed texture folder Reduce Cvm Map bitrate to 2 bits

--- trunk/CvmFight/CvmFight/Viewers/GameViewer3D/SpriteViewer/SpriteViewer3D.cs
+++ trunk/CvmFight/CvmFight/Viewers/GameViewer3D/SpriteViewer/SpriteViewer3D.cs
@@ -40,7 +40,7 @@ namespace CvmFight
             spriteCollectionCache3D = new SpriteCollectionCache3D(spritePool, isEnableSpriteCache, isEnableLazySpriteImageLoad);
 
             splatCache3D = new SpriteCache3D(isEnableSpriteCache, isEnableLazySpriteImageLoad);
-            splatCache3D.AddFrame(new SpriteScallableFrame(SpriteScallableFrame.Undefined, SpriteScallableFrame.Undefined, "Assets/Textures/Sprites/Splats/splat001.png", isEnableLazySpriteImageLoad));
+            splatCache3D.AddFrame(new SpriteScallableFrame(SpriteScallableFrame.Undefined, SpriteScallableFrame.Undefined, "Assets/Sprites/Splats/splat001.png", isEnableLazySpriteImageLoad));
             energyBarViewer = new EnergyBarViewer();
         }
         #endregion
